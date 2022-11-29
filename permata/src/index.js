43b4269c5529bcf4ff66c96e79b7aa7f0894f690
gravicalcoder <thatdaythatdaywhat@gmail.com>
2022-11-29 14:41:32 +0000

    "pake yang standar"

---
+++ permata/src/index.js
@@ -0,0 +1,41 @@
+
+import { createRoot } from 'react-dom/client'
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+  <div>
+  <h1>Hello React</h1>
+  <p>harus standar</p>
+</div>
+)
+
+
+
+/*
+import React from 'react';
+import {createroot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<h1>Hello World!</h1>);
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+*/
